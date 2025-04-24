274012400ee3d3c1f8a2d2425ce09c2285538f98
HansBerkvens <hans_berkvens@hotmail.com>
2025-04-24 17:28:32 +0200

    auto check if driver instances are independent

--- Webdriver/__init__.py
+++ Webdriver/__init__.py
@@ -7,16 +7,14 @@ from queue import Queue
 
 
 def open_drivers(amt_drivers: int, new_driver_startup_time: float = 1.0, **kwargs) -> list[Driver] | Driver:
-    def create_driver(queue: Queue, index: int):
-        sleep((1+index) * new_driver_startup_time)
-        driver = Driver(offset_index=index, **kwargs)
-        queue.put(driver)
-
     print(f'opening {amt_drivers} drivers')
 
     if amt_drivers == 1:
         return Driver(offset_index=0, **kwargs)
-
+    def create_driver(queue: Queue, index: int):
+        sleep((1+index) * new_driver_startup_time)
+        driver = Driver(offset_index=index, **kwargs)
+        queue.put(driver)
     queue = Queue()
     threads = []
 
@@ -29,6 +27,22 @@ def open_drivers(amt_drivers: int, new_driver_startup_time: float = 1.0, **kwarg
         t.join()
 
     drivers = [queue.get() for _ in range(amt_drivers)]
+
+    # check if all instances are independent
+    window_handles = set()
+    for d in drivers:
+        window_handles.add(tuple(d.driver.window_handles))
+
+    # add logic if instances are referring to the same driver
+    if len(window_handles) != amt_drivers:
+        print(f'Multiple Driver instances are sharing the same driver, retrying with increased stagger time for independence.')
+        # kill available drivers
+        for d in drivers:
+            d.quit()
+
+        # re-try with longer staggering time
+        return open_drivers(amt_drivers, new_driver_startup_time+1.5, **kwargs)
+
     return drivers
 
 
